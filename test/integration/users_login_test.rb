dc6598c50ea12e89f4ef2c107d0f641970fe7790
takuya-38 <takuya.kogoma.38@gmail.com>
2023-12-25 06:10:25 +0900

    feat: ログアウト機能追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,18 +5,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "login with invalid information" do
+  test "login with valid email/invalid password" do
     get login_path
     assert_template "sessions/new"
     post login_path, params: { session: { email:    @user.email,
                                             password: "invalid" } }
+    assert_not is_logged_in?
     assert_response :unprocessable_entity
     assert_template "sessions/new"
   end
 
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     post login_path, params: { session: { email:    @user.email,
                                           password: "password" } }
+    assert_predicate(self, :is_logged_in?)
     assert_redirected_to root_path
     follow_redirect!
     assert_template root_path
@@ -25,5 +27,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", editor_path
     assert_select "a[href=?]", edit_user_path(@user)
     assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", new_user_path
+    assert_select "a[href=?]", editor_path, count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
