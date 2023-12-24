9259384f975012d47c97c76296823ce7107d67ad
takuya-38 <takuya.kogoma.38@gmail.com>
2023-12-25 03:23:30 +0900

    add: generate Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root "article#home"
   get "article/home"
   get "article/show"
@@ -6,5 +7,8 @@ Rails.application.routes.draw do
   get "/register", to: "users#new"
   get "/setting", to: "users#edit"
   get "/profile", to: "users#show"
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
   resources :users
 end
