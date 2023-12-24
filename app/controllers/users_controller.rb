3ab267bc685f93ba93ebe259e5420819b7c36737
takuya-38 <takuya.kogoma.38@gmail.com>
2023-12-25 03:13:30 +0900

    feat: ユーザー登録

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    # @user = User.find(prams[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get "article/home"
   get "article/show"
   get "article/create"
-  get "/signup", to: "users#new"
+  get "/register", to: "users#new"
+  get "/setting", to: "users#edit"
+  get "/profile", to: "users#show"
+  resources :users
 end
